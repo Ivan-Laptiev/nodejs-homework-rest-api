5df5837e95d6dafaea0c99537f9253306bbee2e4
Ivan-Laptiev <laptiev.ivan.j@gmail.com>
2023-01-12 13:46:54 +0200

    fix1

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,20 +10,22 @@ const authenticate = async (req, res, next) => {
 	try {
 		const { authorization = '' } = req.headers;
 		const [bearer = '', token = ''] = authorization.split(' ');
-		if (bearer !== 'Bearer') {
-			throw HttpError(401);
+		
+		if(!token) {
+			throw HttpError(401, 'Not authorized')
 		}
-		try {
-			const { id } = jwt.verify(token, SECRET_KEY);
-			const user = await User.findById(id);
-			if (!user || !user.token) {
-				throw Error('Not authorized');
-			}
-			req.user = user;
-			next();
-		} catch (error) {
+
+		if (bearer !== 'Bearer') {
 			throw HttpError(401, 'Not authorized');
 		}
+		
+		const { id } = jwt.verify(token, SECRET_KEY);
+		const user = await User.findById(id);
+		if (!user || !user.token) {
+			throw HttpError(401, 'Not authorized');
+		}
+		req.user = user;
+		next();		
 	} catch (error) {
 		next(error);
 	}
